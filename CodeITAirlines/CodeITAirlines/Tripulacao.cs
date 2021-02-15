b1e70a92ef16dfc921db7008c997290a8fd7517a
guqueirozzz <43295921+guqueirozzz@users.noreply.github.com>
2021-02-15 20:49:32 -0300

    Update Tripulacao.cs

--- CodeITAirlines/CodeITAirlines/Tripulacao.cs
+++ CodeITAirlines/CodeITAirlines/Tripulacao.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,16 +10,19 @@ namespace CodeITAirlines
         public int index { get; set; }
         public int permissao_conducao { get; set; }
         public string descricao { get; set; }
+        public int fl_policia { get; set; }
 
         // Construtor padrão
         public Tripulacao()
         { }
 
-       public Tripulacao(int index, int permissao_conducao, string descricao)
+       public Tripulacao(int index, int permissao_conducao, string descricao, int fl_policia)
         {
             this.index = index;
             this.permissao_conducao = permissao_conducao;
             this.descricao = descricao;
+            this.fl_policia = fl_policia;
+
         }
 
         //Função para carregas as pessoas para dar inicio ao prgrama
@@ -27,14 +30,14 @@ namespace CodeITAirlines
         {
             List<Tripulacao> tripulacao_lista = new List<Tripulacao>();
 
-            tripulacao_lista.Add(new Tripulacao(0, 1, "0 - Piloto"));
-            tripulacao_lista.Add(new Tripulacao(1, 0, "1 - Oficial 01"));
-            tripulacao_lista.Add(new Tripulacao(2, 0, "2 - Oficial 02"));
-            tripulacao_lista.Add(new Tripulacao(3, 1, "3 - Chefe de Serviço"));
-            tripulacao_lista.Add(new Tripulacao(4, 0, "4 - Comissaria 01"));
-            tripulacao_lista.Add(new Tripulacao(5, 0, "5 - Comissaria 02"));
-            tripulacao_lista.Add(new Tripulacao(6, 1, "6 - Policial"));
-            tripulacao_lista.Add(new Tripulacao(7, 0, "7 - Presidiário"));
+            tripulacao_lista.Add(new Tripulacao(index = 0, 1, "0 - Piloto", 0));
+            tripulacao_lista.Add(new Tripulacao(index = 1, 0, "1 - Oficial 01", 0));
+            tripulacao_lista.Add(new Tripulacao(index = 2, 0, "2 - Oficial 02", 0));
+            tripulacao_lista.Add(new Tripulacao(index = 3, 1, "3 - Chefe de Serviço", 0));
+            tripulacao_lista.Add(new Tripulacao(index = 4, 0, "4 - Comissaria 01", 0));
+            tripulacao_lista.Add(new Tripulacao(index = 5, 0, "5 - Comissaria 02", 0));
+            tripulacao_lista.Add(new Tripulacao(index = 6, 1, "6 - Policial", 1));
+            tripulacao_lista.Add(new Tripulacao(index = 7, 0, "7 - Presidiário", 0));
            
 
             return tripulacao_lista;
