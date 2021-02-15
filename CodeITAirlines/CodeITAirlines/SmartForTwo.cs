dbec7e9f1bd28d25da17adee331649a8c136085e
guqueirozzz <43295921+guqueirozzz@users.noreply.github.com>
2021-02-15 20:48:31 -0300

    Update SmartForTwo.cs

--- CodeITAirlines/CodeITAirlines/SmartForTwo.cs
+++ CodeITAirlines/CodeITAirlines/SmartForTwo.cs
@@ -1,12 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CodeITAirlines
 {
-    class SmartForTwo : Aviao // uso de Herança
+    class SmartForTwo  // uso de Herança
     {
         Terminal term = new Terminal();
+        Aviao aviao = new Aviao();
+
         public bool validaPessoas(int idx_motorista, int idx_passageiro, List<Tripulacao> tripulacao)
         {
             string motorista = tripulacao[idx_motorista].descricao;
@@ -19,22 +21,22 @@ namespace CodeITAirlines
             }
             else if (tripulacao[idx_passageiro].index == 1 && tripulacao[idx_motorista].index == 3)
             {
-                Console.WriteLine("O Oficial 01 não pode viajar com o chefe de serviço");
+                Console.WriteLine(passageiro + " não pode viajar com o " + motorista);
                 return false;
             }
             else if (tripulacao[idx_passageiro].index == 2 && tripulacao[idx_motorista].index == 3)
             {
-                Console.WriteLine("O " + passageiro +  "não pode viajar com o " + motorista);
+                Console.WriteLine( passageiro +  " não pode viajar com o " + motorista);
                 return false;
             }
             else if (tripulacao[idx_passageiro].index == 4 && tripulacao[idx_motorista].index == 0) 
             {
-                Console.WriteLine("O " + passageiro + "não pode viajar com o " + motorista);
+                Console.WriteLine( passageiro + " não pode viajar com o " + motorista);
                 return false;
             }
             else if (tripulacao[idx_passageiro].index == 5 && tripulacao[idx_motorista].index == 0)
             {
-                Console.WriteLine("O " + motorista + "não pode ficar sozinha com a" + passageiro);
+                Console.WriteLine( passageiro + " não pode viajar com o " + motorista);
                 return false;
             }
             else
@@ -55,8 +57,8 @@ namespace CodeITAirlines
                 Console.WriteLine("Trajeto: " + motorista + " leva " + passageiro + " do terminal até o avião.");
                 term.RmPessoasTerminal(idx_motorista, tripulacao);
                 term.RmPessoasTerminal(idx_passageiro, tripulacao);
-                AddPessoasAviao(idx_passageiro, tripulacao);
-                AddPessoasAviao(idx_motorista, tripulacao);
+                aviao.AddPessoasAviao(idx_passageiro, tripulacao);
+                aviao.AddPessoasAviao(idx_motorista, tripulacao);
             }
         }
 
@@ -68,13 +70,14 @@ namespace CodeITAirlines
                 Console.WriteLine("O " + motorista + " não tem permissão para condução do veiculo");
 
             else if (tripulacao.Count == 1 && tripulacao[idx_motorista].permissao_conducao == 1 )
-                AddPessoasAviao(idx_motorista, tripulacao);
+                aviao.AddPessoasAviao(idx_motorista, tripulacao);
 
             else if (tripulacao[idx_motorista].permissao_conducao == 1)
             {
                 Console.WriteLine("O " + motorista + " irá sozinho ate o terminal");
-                RmPessoasAviao(idx_motorista, tripulacao);
+                aviao.RmPessoasAviao(idx_motorista, tripulacao);
                 term.AddPessoasTerminal(idx_motorista, tripulacao);
+
             }
         }
     }
